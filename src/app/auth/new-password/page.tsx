cc89a4fcaf955cdb97d4e52868fadb93766475d3
agung rosyandi <agungrosyandi@gmail.com>
2025-05-04 08:37:14 +0700

    update 4 may 2025 8

--- src/app/auth/new-password/page.tsx
+++ src/app/auth/new-password/page.tsx
@@ -1,12 +1,16 @@
+import Loading from "@/app/(content)/list-destination/[places]/loading";
 import LoginFormBackgroundImage from "@/components/auth/login-form-background-image";
 import NewPasswordForm from "@/components/auth/new-password-form";
 import AuthContainer from "@/components/reusable/auth-container";
+import { Suspense } from "react";
 
 export default function NewPass() {
   return (
     <AuthContainer>
-      <LoginFormBackgroundImage />
-      <NewPasswordForm />;
+      <Suspense fallback={<Loading />}>
+        <LoginFormBackgroundImage />
+        <NewPasswordForm />;
+      </Suspense>
     </AuthContainer>
   );
 }
--- src/app/auth/new-verification/page.tsx
+++ src/app/auth/new-verification/page.tsx
@@ -1,12 +1,16 @@
+import Loading from "@/app/(content)/list-destination/[places]/loading";
 import EmailVerificationForm from "@/components/auth/email-verification-form";
 import LoginFormBackgroundImage from "@/components/auth/login-form-background-image";
 import AuthContainer from "@/components/reusable/auth-container";
+import { Suspense } from "react";
 
 export default function EmailVerification() {
   return (
     <AuthContainer>
-      <LoginFormBackgroundImage />
-      <EmailVerificationForm />;
+      <Suspense fallback={<Loading />}>
+        <LoginFormBackgroundImage />
+        <EmailVerificationForm />;
+      </Suspense>
     </AuthContainer>
   );
 }
